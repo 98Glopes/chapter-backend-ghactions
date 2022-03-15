615638e2cb427d0bba0e2fb841c0766abaec0df0
Gabriel <gabriel.lopes@outlook.com>
2022-03-15 20:26:05 -0300

    add base app

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+
+def test_esse_deve_passar():
+    """
+    Durante a fase de CI este teste deve passar
+    """
+    assert True == True
+
+
+def test_esse_deve_falhar():
+    """
+    Durante a fase de CI este teste deve falhar
+    """
+    assert False == True
